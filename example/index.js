9276e36741de9cd0a70787f354c5e7c1f76218dd
Phayden <fruitgusherseater@icloud.com>
2020-06-16 23:16:30 -0400

    Added example

---
+++ example/index.js
@@ -0,0 +1,19 @@
+// jshint esversion: 6
+
+// All possible functions
+const { fromFile, objSize, toCSS, minify } = require('json-reader');
+
+// files
+const jsonc_file = "example.jsonc"; // .json will work the same as well
+const jsonstyle_file = "style.jsonc";
+
+var json_obj = fromFile(jsonc_file); // This will return a object from the example.jsonc file
+var css = toCSS(fromFile(jsonstyle_file)); // This returns a minified css string from the contents of "style.jsonc"
+var obj_size = objSize(json_obj); // Will return 2
+var minified = minify(json_obj); // This will turn the object into a string and minify it, it will also minify json strings that are formatted
+
+// Print to console
+console.log(json_obj);
+console.log(css);
+console.log(obj_size);
+console.log(minified);
--- index.js
+++ index.js
@@ -15,18 +15,6 @@ class storage {
 var info = new storage();
 //#endregion
 
-//#region Check if valid url
-function validURL(str) {
-    var pattern = new RegExp('^(https?:\\/\\/)?' +
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' +
-        '((\\d{1,3}\\.){3}\\d{1,3}))' +
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' +
-        '(\\?[;&a-z\\d%_.~+=-]*)?' +
-        '(\\#[-a-z\\d_]*)?$', 'i');
-    return !!pattern.test(str);
-}
-//#endregion
-
 //#region From File
 exports.fromFile = function (file) {
 
